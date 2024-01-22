2d4e4c654443d95a059373c7910e37d0c11ec73e
MrSakine <msakine20@gmail.com>
2024-01-22 15:09:15 -0800

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# Install Nginx web server
+
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => 'server {
+    listen 80;
+    listen [::]:80;
+    server_name _;
+    location / {
+        root /var/www/html;
+        index index.html;
+    }
+    location /redirect_me {
+        return 301 https://www.google.com/;
+    }
+}',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
