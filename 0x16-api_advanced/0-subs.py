d5e789cc2c5d14fb96093b3dea1485e938e7735d
MrSakine <115190839+MrSakine@users.noreply.github.com>
2024-03-05 12:37:21 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns total subscribers of a sub reddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditBot/1.0"}
+    headers = {"User-Agent": "MyRedditBot/1.0 (fily)"}
     response = requests.get(url=url, headers=headers, allow_redirects=False)
     if response.status_code == 404 or response.status_code == 302:
         return 0
