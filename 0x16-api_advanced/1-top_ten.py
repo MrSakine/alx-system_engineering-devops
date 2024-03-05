0d4f3a0eba203d8cbd022cce024afae765751388
MrSakine <msakine20@gmail.com>
2024-03-05 12:33:45 +0000

    the top ten of a sub reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Return the top ten of a sub reddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'MyBot/1.0 (your_username)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("error"):
+            print(None)
+        else:
+            data = data.get("data")
+            for _, post in enumerate(data.get("children"), start=1):
+                res = post.get("data")
+                print("{}".format(res.get("title")))
+    else:
+        print(None)
